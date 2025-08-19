51b989bc2a68ae21cf6119efaff8bc15693372fc
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-19 06:36:27 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,1 +1,14 @@
+import sqlite3
 
+def get_db_connection():
+    conn = sqlite3.connect('safevault.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def query_user_by_email(email):
+    conn = get_db_connection()
+    cursor = conn.execute("SELECT * FROM users WHERE email = ?", (email,))
+    user = cursor.fetchone()
+    conn.close()
+    return user
+  
