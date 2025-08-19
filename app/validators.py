b4c6b0f1fcd4b062958ba2327be43ed7f90fcc41
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-19 06:38:14 +0100

    Update validators.py

--- app/validators.py
+++ app/validators.py
@@ -1,1 +1,9 @@
+import re
 
+def is_valid_email(email):
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, email)
+
+def is_strong_password(password):
+    return len(password) >= 8 and any(char.isdigit() for char in password)
+  
